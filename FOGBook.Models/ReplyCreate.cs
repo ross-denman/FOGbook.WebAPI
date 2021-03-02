5d66b27369427bddfd53f1fc23585050655e96e4
Bob Madison <robertdavidmadison@gmail.com>
2021-03-02 07:46:23 -0500

    Finish CreateReply and Reply Service

--- FOGBook.Models/ReplyCreate.cs
+++ FOGBook.Models/ReplyCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,15 @@ namespace FOGBook.Models
 {
     public class ReplyCreate
     {
-        [Required]
-        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(8000, ErrorMessage = "There are too many characters in this field.")]
         public string Text { get; set; }
+
+        public class CommentCreate
+        {
+            [Required]
+            [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+            [MaxLength(8000, ErrorMessage = "There are too many characters in this field.")]
+            public string Text { get; set; }
+
+        }
     }
 }
